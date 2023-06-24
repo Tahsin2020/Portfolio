40075e91d8841fdc94d41f6a3b1e52a405f0865f
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-06-23 22:40:45 -0700

    putting dist into vue config

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   // publicPath: "/Portfolio/",
   // transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
 });
 // vue.config.js file to be placed in the root of your repository
