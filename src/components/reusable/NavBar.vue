dcbb007293da0877819a845d48311b82e4a55734
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-06-26 12:46:51 -0700

    Portfolio is complete, can add info as I wish

--- src/components/reusable/NavBar.vue
+++ src/components/reusable/NavBar.vue
@@ -6,8 +6,8 @@
         <span class="self-center text-2xl font-semibold whitespace-nowrap text-white">Tahsin Hasan</span>
       </router-link>
       <button data-collapse-toggle="navbar-default" type="button"
-        class="inline-flex items-center p-2 ml-3 text-sm text-white rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
-        aria-controls="navbar-default" aria-expanded="false" @click="controlnav">
+        class="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+        aria-controls="navbar-default" aria-expanded="false" @click="update">
         <span class="sr-only">Open main menu</span>
         <svg class="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20"
           xmlns="http://www.w3.org/2000/svg">
@@ -16,7 +16,7 @@
             clip-rule="evenodd"></path>
         </svg>
       </button>
-      <div class="w-full md:block md:w-auto" v-if="view" id="navbar-default">
+      <div class="w-full md:block md:w-auto" v-show="value1" id="navbar-default">
         <ul
           class="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 md:mt-0 md:border-0">
           <router-link to="/Bio">
@@ -46,17 +46,18 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-const view = ref(true)
 
 export default {
   name: "NavBar",
-  data: function () {
-    return view;
+  data() {
+    return {
+      value1: true
+    }
   },
   methods: {
-    controlnav: function () {
-
+    update: function () {
+      console.log(this.value1)
+      this.value1 = !this.value1
     }
   }
 };
