3e7f46173683d6be7dc8326ef36319889728923c
Tahsin Hasan <tahsinhasan2@gmail.com>
2023-11-06 12:50:57 -0800

    Destroy

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  // publicPath: "/Portfolio/",
-  // transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/Portfolio/" : "/",
-});
-// vue.config.js file to be placed in the root of your repository
