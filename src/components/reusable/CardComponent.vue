437fe7eed3b2a4be0e6375e8b3268339570f5b94
Tahsin Hasan <thason@ttgteams.com>
2026-05-22 07:55:51 -0700

    Made some Visual changes

--- src/components/reusable/CardComponent.vue
+++ src/components/reusable/CardComponent.vue
@@ -18,15 +18,15 @@ console.log(props)
 </script>
 <template>
     <div class="shadow-md rounded-lg w-80 py-2 mx-10  mb-[10vh] bg-[#444444] " :class="Class">
-        <div v-if="Link" class=" h-full">
+        <div v-if="Link"  class="flex flex-col content-between h-full" >
             <a :href="Link">
                 <img :src="Image" class="max-h-40 mx-auto" />
             </a>
             <h2 class="mt-4 text-white text-center"> {{ Description }}</h2><br />
             <h3 class="mt-4 text-slate-300 text-center mt-auto"> {{ Position }}</h3>
         </div>
-        <div v-else> 
-                <img :src="Image" class="max-h-40 mx-auto" />\
+        <div class="flex flex-col content-between h-full" v-else> 
+            <img :src="Image" class="max-h-40 mx-auto" />
             <h2 class="mt-4 text-white text-center"> {{ Description }}</h2>
             <h3 class="mt-4 text-slate-300 text-center mt-auto"> {{ Position }}</h3>
         </div>
